d2a1fe77b0e134538d1f82c50d18ae13dad0eaa7
njl07 <akanass@icloud.com>
2017-11-13 16:53:59 +0100

    feat(module): Fix store + manage mutable states

--- src/lib/shared/services/transfer-http-cache-interceptor/transfer-http-cache.interceptor.ts
+++ src/lib/shared/services/transfer-http-cache-interceptor/transfer-http-cache.interceptor.ts
@@ -1,10 +1,13 @@
-import { ApplicationRef, Injectable } from '@angular/core';
+import { ApplicationRef, Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { TransferState, makeStateKey, StateKey } from '@angular/platform-browser';
+import { isPlatformServer } from '@angular/common';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { first, filter, flatMap, map, tap } from 'rxjs/operators';
+import { _throw } from 'rxjs/observable/throw';
+import { from } from 'rxjs/observable/from';
+import { first, filter, flatMap, map, tap, defaultIfEmpty } from 'rxjs/operators';
 import { mergeStatic } from 'rxjs/operators/merge';
 
 import * as createHash from 'create-hash/browser';
@@ -20,22 +23,37 @@ interface TransferHttpResponse {
     url?: string;
 }
 
+/**
+ * Server state interface
+ */
+interface ServerStateData {
+    id: number,
+    reqKey: string
+}
+
 @Injectable()
 export class TransferHttpCacheInterceptor implements HttpInterceptor {
     // private property to store cache activation status
     private _isCacheActive: boolean;
     // private property to store unique id of the key
     private _id: number;
+    // private property to store serve state data store key
+    private _serverStateDataStoreKey: StateKey<ServerStateData[]>;
+    // private property to store last id store key
+    private _lastIdStoreKey: StateKey<number>;
 
     /**
      * Class constructor
      *
      * @param {ApplicationRef} _appRef
      * @param {TransferState} _transferState
+     * @param {Object} _platformId
      */
-    constructor(private _appRef: ApplicationRef, private _transferState: TransferState) {
+    constructor(private _appRef: ApplicationRef, private _transferState: TransferState, @Inject(PLATFORM_ID) private _platformId: Object) {
         this._isCacheActive = true;
         this._id = 0;
+        this._serverStateDataStoreKey = makeStateKey<ServerStateData[]>('server_state_data');
+        this._lastIdStoreKey = makeStateKey<number>('server_state_last_id');
 
         // Stop using the cache if the application has stabilized, indicating initial rendering is complete.
         this._appRef.isStable
@@ -63,7 +81,11 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
                         isCacheActive
                             .pipe(
                                 filter(_ => !_),
-                                flatMap(_ => next.handle(req))
+                                flatMap(_ => next.handle(req)
+                                    .pipe(
+                                        tap(__ => this._cleanServerState())
+                                    )
+                                )
                             ),
                         isCacheActive
                             .pipe(
@@ -73,6 +95,34 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
                     )
                 )
             );
+    }
+
+    /**
+     * Function to clean all data in server state
+     *
+     * @private
+     */
+    private _cleanServerState(): void {
+        mergeStatic(
+            this._getLastId(false)
+                .pipe(
+                    tap(_ => this._transferState.remove(this._lastIdStoreKey))
+                ),
+            this._getServerStateData(false)
+                .pipe(
+                    tap(_ =>
+                        _.forEach(__ =>
+                            this._transferState.remove(makeStateKey<TransferHttpResponse>(this._createHash(`${__.reqKey}_${__.id}`)))
+                        )
+                    ),
+                    tap(_ => this._transferState.remove(this._serverStateDataStoreKey))
+                )
+        ).subscribe(
+            undefined,
+            e => {
+                throw(e);
+            }
+        );
     }
 
     /**
@@ -114,10 +164,209 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
     private _createKey(req: HttpRequest<any>): Observable<StateKey<TransferHttpResponse>> {
         this._id++;
 
-        return of(this._id)
+        return of(of(isPlatformServer(this._platformId)))
             .pipe(
-                map(id => createHash('sha256').update(`${JSON.stringify(req)}_${id}`).digest('hex')),
+                flatMap(isServer =>
+                    mergeStatic(
+                        isServer
+                            .pipe(
+                                filter(_ => !!_),
+                                flatMap(_ => this._serverKey(req))
+                            ),
+                        isServer
+                            .pipe(
+                                filter(_ => !_),
+                                flatMap(_ => this._clientKey(req))
+                            )
+                    )
+                )
+            );
+    }
+
+    /**
+     * Function to get state data and create client key for current request
+     *
+     * @param {HttpRequest<any>} req
+     *
+     * @return {Observable<StateKey<TransferHttpResponse>>}
+     *
+     * @private
+     */
+    private _clientKey(req: HttpRequest<any>): Observable<StateKey<TransferHttpResponse>> {
+        return of(this._createHash(JSON.stringify(req)))
+            .pipe(
+                flatMap(reqKey =>
+                    this._getServerStateData()
+                        .pipe(
+                            flatMap(_ => from(_)),
+                            filter(_ => _.reqKey === reqKey),
+                            defaultIfEmpty(undefined),
+                            flatMap(_ =>
+                                !!_ ?
+                                    of(_) :
+                                    _throw(new Error('Request missing in server state data'))
+                            ),
+                            flatMap(serverState =>
+                                this._getLastId()
+                                    .pipe(
+                                        flatMap(_ =>
+                                            !_ || this._id > _ ?
+                                                _throw(new Error('Wrong id for server state data')) :
+                                                of(this._id)
+                                        ),
+                                        map(_ => _ === serverState.id ? _ : serverState.id),
+                                        map(id => this._createHash(`${reqKey}_${id}`)),
+                                        map(key => makeStateKey<TransferHttpResponse>(key))
+                                    )
+                            )
+                        )
+                )
+            );
+    }
+
+    /**
+     * Function to get last id from server
+     *
+     * @param {boolean} throwError
+     *
+     * @return {Observable<number>}
+     *
+     * @private
+     */
+    private _getLastId(throwError: boolean = true): Observable<number> {
+        return of(this._lastIdStoreKey)
+            .pipe(
+                flatMap(storeKey =>
+                    of(of(this._transferState.hasKey(storeKey)))
+                        .pipe(
+                            flatMap(hasKey =>
+                                mergeStatic(
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !_),
+                                            flatMap(_ =>
+                                                of(throwError)
+                                                    .pipe(
+                                                        filter(__ => !!__),
+                                                        flatMap(__ => _throw(new Error('Missing server state last id')))
+                                                    )
+                                            )
+                                        ),
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !!_),
+                                            map(_ => this._transferState.get(storeKey, 0))
+                                        )
+                                )
+                            )
+                        )
+                )
+            )
+    }
+
+    /**
+     * Function to get server state data
+     *
+     * @param {boolean} throwError
+     *
+     * @return {Observable<ServerStateData[]>}
+     *
+     * @private
+     */
+    private _getServerStateData(throwError: boolean = true): Observable<ServerStateData[]> {
+        return of(this._serverStateDataStoreKey)
+            .pipe(
+                flatMap(storeKey =>
+                    of(of(this._transferState.hasKey(storeKey)))
+                        .pipe(
+                            flatMap(hasKey =>
+                                mergeStatic(
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !_),
+                                            flatMap(_ =>
+                                                of(throwError)
+                                                    .pipe(
+                                                        filter(__ => !!__),
+                                                        flatMap(__ => _throw(new Error('Missing server state data')))
+                                                    )
+                                            )
+                                        ),
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !!_),
+                                            map(_ => this._transferState.get(storeKey, [] as ServerStateData[]))
+                                        )
+                                )
+                            )
+                        )
+                )
+            )
+    }
+
+    /**
+     * Function to create server key and store state data for current request
+     *
+     * @param {HttpRequest<any>} req
+     *
+     * @return {Observable<StateKey<TransferHttpResponse>>}
+     *
+     * @private
+     */
+    private _serverKey(req: HttpRequest<any>): Observable<StateKey<TransferHttpResponse>> {
+        return of(this._createHash(JSON.stringify(req)))
+            .pipe(
+                tap(reqKey => this._storeServerStateData(reqKey)),
+                map(reqKey => this._createHash(`${reqKey}_${this._id}`)),
                 map(key => makeStateKey<TransferHttpResponse>(key))
+            );
+    }
+
+    /**
+     * Function to store server state data
+     *
+     * @param {string} reqKey
+     *
+     * @private
+     */
+    private _storeServerStateData(reqKey: string): void {
+        of(this._serverStateDataStoreKey)
+            .pipe(
+                flatMap(storeKey =>
+                    of(of(this._transferState.hasKey(storeKey)))
+                        .pipe(
+                            flatMap(hasKey =>
+                                mergeStatic(
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !_),
+                                            map(_ => [] as ServerStateData[])
+                                        ),
+                                    hasKey
+                                        .pipe(
+                                            filter(_ => !!_),
+                                            map(_ => this._transferState.get(storeKey, [] as ServerStateData[])),
+                                            flatMap(serverStateData =>
+                                                !!serverStateData.find(_ => _.reqKey === reqKey) ?
+                                                    _throw(new Error('Request already stored in server state data')) :
+                                                    of(serverStateData)
+                                            )
+                                        )
+                                )
+                            ),
+                            tap(_ => this._transferState.set(this._lastIdStoreKey, this._id))
+                        )
+                )
+            )
+            .subscribe(
+                serverStateData => this._transferState.set(this._serverStateDataStoreKey, serverStateData.concat({
+                        id: this._id,
+                        reqKey
+                    })
+                ),
+                e => {
+                    throw(e);
+                }
             );
     }
 
@@ -171,15 +420,15 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
                             tap((event: HttpEvent<any>) =>
                                 of(event)
                                     .pipe(
-                                        filter(evt => evt instanceof HttpResponse),
-                                        tap((evt: HttpResponse<any>) => this._transferState.set(storeKey, {
-                                            body: evt.body,
-                                            headers: this._getHeadersMap(evt.headers),
-                                            status: evt.status,
-                                            statusText: evt.statusText,
-                                            url: evt.url!,
-                                        }))
+                                        filter(evt => evt instanceof HttpResponse)
                                     )
+                                    .subscribe((evt: HttpResponse<any>) => this._transferState.set(storeKey, {
+                                        body: evt.body,
+                                        headers: this._getHeadersMap(evt.headers),
+                                        status: evt.status,
+                                        statusText: evt.statusText,
+                                        url: evt.url!,
+                                    }))
                             )
                         )
                 )
@@ -197,5 +446,18 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
      */
     private _getHeadersMap(headers: HttpHeaders): { [name: string]: string[] } {
         return headers.keys().reduce((acc, curr) => Object.assign(acc, { [curr]: headers.getAll(curr)! }), {});
+    }
+
+    /**
+     * Function to create sha256 hash
+     *
+     * @param data
+     *
+     * @return {string}
+     *
+     * @private
+     */
+    private _createHash(data: any): string {
+        return createHash('sha256').update(data).digest('hex');
     }
 }
